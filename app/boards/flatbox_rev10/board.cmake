44cf7154b6139e2d8c997d734cee9630a62beea2
Jacek Fedorynski <jfedor@jfedor.org>
2026-07-09 15:29:34 +0200

    Add Flatbox rev10 build

---
+++ app/boards/flatbox_rev10/board.cmake
@@ -0,0 +1,12 @@
+board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
+#board_runner_args(pyocd "--target=stm32g0b1kbux")
+#board_runner_args(pyocd "--flash-opt=-O reset_type=hw")
+#board_runner_args(pyocd "--flash-opt=-O connect_mode=under-reset")
+#board_runner_args(jlink "--device=STM32G0B1KB" "--speed=4000")
+#board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+
+include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
+#include(${ZEPHYR_BASE}/boards/common/openocd-stm32.board.cmake)
+#include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+#include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+#include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
